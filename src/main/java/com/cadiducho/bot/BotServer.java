a22fc5c0589bddc22d9ae9bb78f3dd264168a2fd
Cadiducho <cadiducho@gmail.com>
2018-06-25 17:08:55 +0200

    Actualizar dependencias
    Versión 2.0

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 public class BotServer {
 
-    public static final String VERSION = "2.0-beta4";
+    public static final String VERSION = "2.0";
     
     @Getter static boolean running = true;
     private static final Scanner in = new Scanner(System.in);
