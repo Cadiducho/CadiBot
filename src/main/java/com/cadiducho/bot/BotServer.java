1040f644bbd4398275e71df9250998f0ffe6a173
Cadiducho <cadiducho@gmail.com>
2018-06-28 20:50:31 +0200

    Empezar nuevo sistema de log
    #3

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -9,23 +9,28 @@ import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class BotServer {
 
-    public static final String VERSION = "2.0";
-    
     @Getter static boolean running = true;
     private static final Scanner in = new Scanner(System.in);
+    /**
+     * The logger for this server
+     */
+    public static final Logger logger = Logger.getLogger("Cadibot-Server");
+
+    /**
+     * Server / bot version
+     */
+    public static final String VERSION = "2.1-beta";
 
     @Getter private static BotServer instance;
     @Getter private final ModuleManager moduleManager;
     @Getter private final CommandManager commandManager;
     @Getter private MySQL mysql;
     @Getter private TelegramBot cadibot;
-    
-    public static final SimpleDateFormat fulltime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
     public static void main(String[] args) {
         Options options = new Options();
@@ -68,23 +73,23 @@ public class BotServer {
         bot.startServer(cmd);
     }
     
-    public BotServer() {
+    private BotServer() {
         instance = this;
         moduleManager = new ModuleManager(instance, "modules");
         commandManager = new CommandManager(instance);
     }
     
     private void startServer(CommandLine cmd) {
-        System.out.println("Servidor arrancado");
-        
+        logger.info("Servidor arrancado");
+
         cadibot = new TelegramBot(cmd.getOptionValue("token"));
         cadibot.getUpdatesPoller().setOwnerId(Long.parseLong(cmd.getOptionValue("owner")));
         
         try {
             moduleManager.loadModules();
         } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
-            System.out.println("Can't load modules!");
-            System.out.println(ex.getMessage());
+            logger.warning("Can't initialize directory modules!");
+            logger.warning(ex.getMessage());
         }
         
         try {
@@ -96,10 +101,10 @@ public class BotServer {
                     cmd.getOptionValue("database-pass"));
             
             mysql.openConnection();
-            System.out.println("SQL connection established");
+            logger.info("SQL connection established");
         } catch (SQLException ex) {
-            System.out.println("Can't connect to database!");
-            System.out.println(ex.getMessage());
+            logger.warning("Can't connect to database!");
+            logger.warning(ex.getMessage());
         }
         UpdatesHandler events = new UpdatesHandler(cadibot, instance);
         cadibot.getUpdatesPoller().setHandler(events);
@@ -107,8 +112,6 @@ public class BotServer {
         commandManager.load(); //ToDo: ¿Pasar todos a módulos?
         
 
-        System.out.println("Bot " + VERSION + " iniciado");
-        
         while (isRunning()) {
             System.out.println("Elige una opción numérica:"
                 + "\n- stop Salir"
@@ -130,6 +133,8 @@ public class BotServer {
             }
         }
         close();
+
+        logger.info("Bot " + VERSION + " iniciado");
     }
     
     private void close() {
@@ -139,7 +144,7 @@ public class BotServer {
         } catch (SQLException ignored) {
         }
 
-        System.out.println("Terminando...");
+        logger.info("Terminando...");
         System.exit(0);
     }
 }
