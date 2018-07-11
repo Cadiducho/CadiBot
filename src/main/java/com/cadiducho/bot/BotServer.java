c12160723333c5f492340cfd3010798c124364dd
Cadiducho <cadiducho@gmail.com>
2018-07-11 11:01:41 +0200

    Versión 2.1

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -23,7 +23,7 @@ public class BotServer {
     /**
      * Server / bot version
      */
-    public static final String VERSION = "2.1-beta";
+    public static final String VERSION = "2.1";
 
     /**
      * The Module manager
