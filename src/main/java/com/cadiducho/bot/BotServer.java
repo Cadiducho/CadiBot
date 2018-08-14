38cac8f1fed85e26b4621f9ebd4317a793bd53ff
Cadiducho <cadiducho@gmail.com>
2018-08-14 13:08:24 +0200

    Reflejar versión 2.5 beta en el código

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -23,7 +23,7 @@ public class BotServer {
     /**
      * Server / bot version
      */
-    public static final String VERSION = "2.4";
+    public static final String VERSION = "2.5-beta";
 
     /**
      * The Module manager
