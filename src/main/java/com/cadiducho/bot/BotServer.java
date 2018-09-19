6124effa68ecdd96feac3bd2bb982553002a7462
Cadiducho <cadiducho@gmail.com>
2018-09-19 21:06:17 +0200

    start 2.7 dev

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -23,7 +23,7 @@ public class BotServer {
     /**
      * Server / bot version
      */
-    public static final String VERSION = "2.6";
+    public static final String VERSION = "2.7-dev";
 
     /**
      * The Module manager
