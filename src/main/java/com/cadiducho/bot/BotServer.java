9268284714c0c068b2bdedca4026b0f5556a03c8
Cadiducho <cadiducho@gmail.com>
2018-09-25 13:25:41 +0200

    Versión 2.8

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -23,7 +23,7 @@ public class BotServer {
     /**
      * Server / bot version
      */
-    public static final String VERSION = "2.8-beta";
+    public static final String VERSION = "2.8";
 
     /**
      * The Module manager
