2600efdaf76913e8726778db53218afd92fb3667
Cadiducho <cadiducho@gmail.com>
2018-10-15 21:15:23 +0200

    Start 2.10 dev

--- src/main/java/com/cadiducho/bot/BotServer.java
+++ src/main/java/com/cadiducho/bot/BotServer.java
@@ -18,7 +18,7 @@ public class BotServer {
     /**
      * Server / bot version
      */
-    public static final String VERSION = "2.9";
+    public static final String VERSION = "2.10-beta";
 
     /**
      * The Module manager
