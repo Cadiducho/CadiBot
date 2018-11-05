ba5256796579d534a90bdacea99580b46d051924
Cadiducho <cadiducho@gmail.com>
2018-11-05 12:49:22 +0100

    Añadir /cuack

---
+++ src/main/java/com/cadiducho/bot/modules/animales/cmds/DuckCMD.java
@@ -0,0 +1,44 @@
+package com.cadiducho.bot.modules.animales.cmds;
+
+import com.cadiducho.bot.api.command.BotCommand;
+import com.cadiducho.bot.api.command.CommandContext;
+import com.cadiducho.bot.api.command.CommandInfo;
+import com.cadiducho.telegrambotapi.Chat;
+import com.cadiducho.telegrambotapi.Message;
+import com.cadiducho.telegrambotapi.User;
+import com.cadiducho.telegrambotapi.exception.TelegramException;
+import com.squareup.moshi.Moshi;
+import lombok.Getter;
+import lombok.extern.java.Log;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@Log
+@CommandInfo(aliases = {"/duck", "cuack", "/cuack", "pato"})
+public class DuckCMD implements BotCommand {
+
+    private static final String duckAPI = "https://random-d.uk/api/v1/random?type=jpg";
+    private final OkHttpClient httpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+
+    @Override
+    public void execute(final Chat chat, final User from, final CommandContext context, final Integer messageId, final Message replyingTo, Instant instant) throws TelegramException {
+        try (Response response = httpClient.newCall(new Request.Builder().url(duckAPI).build()).execute()) {
+            String duckFile = moshi.adapter(DuckAPIWrapper.class).fromJson(response.body().source()).getUrl();
+            getBot().sendPhoto(chat.getId(), duckFile);
+        } catch (IOException | NullPointerException ex) {
+            getBot().sendMessage(chat.getId(), "No he podido encontrar un pato :(");
+            log.warning("Error procesando un pato");
+            log.warning(ex.getMessage());
+            log.warning(ex.getCause().getMessage());
+        }
+    }
+
+    private static class DuckAPIWrapper {
+        @Getter private String url;
+    }
+}
