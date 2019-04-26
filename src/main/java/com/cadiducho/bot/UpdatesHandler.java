980c225ea5f047e04049db44c683c5d8b52b006c
Cadiducho <cadiducho@gmail.com>
2019-04-26 13:15:59 +0200

    Registrar en el log todo el stacktrace de un error

--- src/main/java/com/cadiducho/bot/UpdatesHandler.java
+++ src/main/java/com/cadiducho/bot/UpdatesHandler.java
@@ -8,6 +8,8 @@ import com.cadiducho.telegrambotapi.handlers.LongPollingHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.ConcurrentModificationException;
@@ -27,27 +29,46 @@ public class UpdatesHandler implements LongPollingHandler {
         }
 
         try {
-            if (update.getMessage().getType().equals(Message.Type.NEW_CHAT_MEMBERS)) {
-                server.getModuleManager().getModules().forEach(m -> m.onNewChatMembers(update.getMessage().getChat(), update.getMessage().getNewChatMembers()));
-            }
-            if (update.getMessage().getType().equals(Message.Type.LEFT_CHAT_MEMBER)) {
-                server.getModuleManager().getModules().forEach(m -> m.onLeftChatMember(update.getMessage().getChat(), update.getMessage().getLeftChatMember()));
-            }
-            if (update.getMessage().getType().equals(Message.Type.TEXT)) {
-                //Si la update fue recibida hace más de 10 minutos, ignorarla
-                if (Instant.ofEpochSecond(update.getMessage().getDate().longValue()).isBefore(Instant.now().minusSeconds(10 * 60))) return;
+            if (update.getMessage() != null) {
+                if (update.getMessage().getType().equals(Message.Type.NEW_CHAT_MEMBERS)) {
+                    server.getModuleManager().getModules().forEach(m -> m.onNewChatMembers(update.getMessage().getChat(), update.getMessage().getNewChatMembers()));
+                }
+                if (update.getMessage().getType().equals(Message.Type.LEFT_CHAT_MEMBER)) {
+                    server.getModuleManager().getModules().forEach(m -> m.onLeftChatMember(update.getMessage().getChat(), update.getMessage().getLeftChatMember()));
+                }
+                if (update.getMessage().getType().equals(Message.Type.TEXT)) {
+                    //Si la update fue recibida hace más de 10 minutos, ignorarla
+                    if (Instant.ofEpochSecond(update.getMessage().getDate().longValue()).isBefore(Instant.now().minusSeconds(10 * 60)))
+                        return;
 
-                boolean success = server.getCommandManager().onCmd(bot, update);
-                server.getModuleManager().getModules().forEach(m -> m.onPostCommand(update, success));
+                    boolean success = server.getCommandManager().onCmd(bot, update);
+                    server.getModuleManager().getModules().forEach(m -> m.onPostCommand(update, success));
+                }
             }
         } catch (TelegramException ex) {
             log.severe("Fallo procesando una Update de la API de Telegram: " + ex.getMessage());
             if (ex.getCause() != null) log.severe("Causa: " + ex.getCause().getMessage());
-            log.severe(Arrays.toString(ex.getStackTrace()));
+
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            ex.printStackTrace(printWriter);
+            log.severe(writer.toString());
         } catch (ConcurrentModificationException ex) {
             log.severe("Fallo de concurrencia procesando una Update de la API de Telegram: " + ex.getMessage());
             if (ex.getCause() != null) log.severe("Causa: " + ex.getCause().getMessage());
-            log.severe(Arrays.toString(ex.getStackTrace()));
+
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            ex.printStackTrace(printWriter);
+            log.severe(writer.toString());
+        } catch (Exception ex) {
+            log.severe("Fallo no esperado procesando una Update de la API de Telegram: " + ex.getMessage());
+            if (ex.getCause() != null) log.severe("Causa: " + ex.getCause().getMessage());
+
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            ex.printStackTrace(printWriter);
+            log.severe(writer.toString());
         }
     }
 }
