f9db421bfffcc45cd1e99dd5ec3072a96ca1d2aa
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-11-19 23:33:50 +0100

    Prohibir a aleix el acceso

--- src/main/java/com/cadiducho/bot/modules/desmotivaciones/cmds/DesmotivacionCMD.java
+++ src/main/java/com/cadiducho/bot/modules/desmotivaciones/cmds/DesmotivacionCMD.java
@@ -9,20 +9,19 @@ import com.cadiducho.zincite.api.command.BotCommand;
 import com.cadiducho.zincite.api.command.CommandContext;
 import com.cadiducho.zincite.api.command.CommandInfo;
 import lombok.extern.java.Log;
+
 import java.time.Instant;
 
 @Log
 @CommandInfo(
-        aliases = {"/desmotivacion", "/desmotivaciones", "/desmotiva"},
-        description = "Obtiene un cartel aleatorio de desmotivaciones.es"
+    aliases = {"/desmotivacion", "/desmotivaciones", "/desmotiva"},
+    description = "Obtiene un cartel aleatorio de desmotivaciones.es"
 )
 public class DesmotivacionCMD implements BotCommand {
     @Override
     public void execute(Chat chat, User user, CommandContext commandContext, Integer integer, Message message, Instant instant) throws TelegramException {
-        String cartel = DesmotivacionesModule.getAPost();
-        if (cartel == null) {
-            getBot().sendMessage(chat.getId(), "No hay carteles en el buffer, inténtalo de nuevo.");
-        } else {
+        if (user.getId() != 124618317L) {
+            String cartel = DesmotivacionesModule.getAPost();
             getBot().sendPhoto(chat.getId(), cartel);
         }
     }
