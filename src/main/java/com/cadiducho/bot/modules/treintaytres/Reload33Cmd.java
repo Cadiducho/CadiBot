2b78a9119e8450a4358d8ccd31ffe7342cf27e09
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2023-05-13 17:55:24 +0200

    Externalizar respuestas de 33 a url

---
+++ src/main/java/com/cadiducho/bot/modules/treintaytres/Reload33Cmd.java
@@ -0,0 +1,33 @@
+package com.cadiducho.bot.modules.treintaytres;
+
+import com.cadiducho.bot.modules.pole.PoleModule;
+import com.cadiducho.telegrambotapi.Chat;
+import com.cadiducho.telegrambotapi.Message;
+import com.cadiducho.telegrambotapi.User;
+import com.cadiducho.telegrambotapi.exception.TelegramException;
+import com.cadiducho.zincite.api.command.BotCommand;
+import com.cadiducho.zincite.api.command.CommandContext;
+import com.cadiducho.zincite.api.command.CommandInfo;
+import lombok.extern.java.Log;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@Log
+@CommandInfo(module = TreintaYTres.class, aliases = "reload33", description = "Recarga las frases de 33")
+public class Reload33Cmd implements BotCommand {
+    @Override
+    public void execute(Chat chat, User user, CommandContext commandContext, Integer integer, Message message, Instant instant) throws TelegramException {
+        if(chat.getId() != -1001696570609L) getBot().sendMessage(chat.getId(), "No tienes permisos para ejecutar este comando");
+        else {
+            try{
+                Respuestas.getInstance().refrescar();
+                getBot().sendMessage(chat.getId(), "Frases recargadas");
+            } catch (IOException e) {
+                getBot().sendMessage(chat.getId(), "Error al recargar las frases:"+e.getMessage());
+            }
+
+
+        }
+    }
+}
--- src/main/java/com/cadiducho/bot/modules/treintaytres/TreintaYTres.java
+++ src/main/java/com/cadiducho/bot/modules/treintaytres/TreintaYTres.java
@@ -2,25 +2,36 @@ package com.cadiducho.bot.modules.treintaytres;
 
 import com.cadiducho.telegrambotapi.Update;
 import com.cadiducho.telegrambotapi.exception.TelegramException;
+import com.cadiducho.telegrambotapi.util.MoshiProvider;
 import com.cadiducho.zincite.ZinciteBot;
+import com.cadiducho.zincite.api.command.CommandManager;
 import com.cadiducho.zincite.api.module.ModuleInfo;
 import com.cadiducho.zincite.api.module.ZinciteModule;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 import com.vdurmont.emoji.EmojiParser;
+import lombok.Getter;
+import lombok.extern.java.Log;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
 
+@Log
 @ModuleInfo(name = "33", description = "cómo?")
 public class TreintaYTres implements ZinciteModule {
 
-    private final List<String> respuestas = Arrays.asList("cómo", "CÓMO", "CÓMO????", "me están filmando", "NONONONONO", "ESTOY DANDO VOLTERETAS", "Eh?", "CÓMO 33?", "me estáis jodiendo de cojones", "Mis cojones 33?", "33? sabes cosas", "Como 33", "Fernando Alonso Díaz (Oviedo, Asturias; 29 de julio de 1981). Ha ganado dos veces el Mundial de Fórmula 1 en 2005 y 2006, una vez el Mundial de Resistencia de la FIA en 2019, las 24 Horas de Le Mans en 2018 y 2019, las 24 Horas de Daytona de 2019 y el Mundial de Karting en 1996.", "Fernando Alonso Díaz. \uD83D\uDC10", "Si hombre jodeme mas", "¿33?", "Filmación.", "esquizofrenia y locura", "COMO TREINTA Y TRES QUE NO QUE NO Y QUE NO \n" + "NANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANONANO", "NONONO", "Camisa de fuerza", "Me voy a hacer 33 pajas para rematar el día", "THE GOAT. \uD83D\uDC10", "Estoy viendo cosas", "33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33\n33".formatted(), "sabe cosas", "Esto es una animalada.", "Como nos lo vamos a pasar...", "Que viene el coco, bufones.", "Esto es demencial", "Histeria colectiva", "Lobi?", "Fernando Alonso va a pelear este mundial. Ahora mismo Red Bull está un paso por delante, pero esa brecha se va a ir cerrando con el paso de los Gps.", "PADRE", "Va a hacer esto sin coñas", "THIS IS A LOVELY CAR TO DRIVE", "Este tío es un enfermo mental y debe ser ingresado en una clínica.", "nadie\n" + "\n" + "absolutamente nadie\n" + "\n" + "el alonsista menos esquizofrénico:", "Esto es una animalada.", "Cuanto hemos estado esperando...", "Va a pelear el mundial", "BYE BYE", "Estoy cagado sin coñas. Tengo un mojonazo en el culo.", "This is a lovely car to drive.", "Este demente lo va a hacer", "Lo va a hacer.", "Cago líquido", "Feliz 2006 queridos amigos", "Me oís gritar?", "Invadiremos Cibeles con la 33", "Perdón?", "Esto no es ni medio normal", "filmando alonso", "OJO", "Y EN NADA, 34");
-
+    private final Random rand = new Random();
     @Override
     public void onPostCommand(Update update, boolean success) {
         final String text = update.getMessage().getText();
         if ((text.contains("33") || text.length() == 33) && !text.contains("http")) {
-            Random rand = new Random();
+            List<String> respuestas = Respuestas.getInstance().getRespuestas();
+
             String reply = EmojiParser.parseToUnicode(respuestas.get(rand.nextInt(respuestas.size())));
             
             try {
@@ -30,4 +41,12 @@ public class TreintaYTres implements ZinciteModule {
             }
         }
     }
+
+    @Override
+    public void onLoad() {
+        log.info("Cargando módulo 33");
+        CommandManager commandManager = ZinciteBot.getInstance().getCommandManager();
+
+    }
+
 }
